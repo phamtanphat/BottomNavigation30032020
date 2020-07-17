9732937442eba9279c51ac8194484aebfc54a9e4
phatdroid94 <phatdroid94@gmail.com>
2020-07-17 20:55:34 +0700

    update code

--- app/src/main/java/com/example/bottomnavigation30032020/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigation30032020/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.bottomnavigation30032020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView mBottom;
+    FragmentManager fragmentManager;
+    HomeFragment homeFragment;
+    ProfileFragment profileFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        mBottom = findViewById(R.id.bottomNavigation);
+        fragmentManager = getSupportFragmentManager();
+
+        homeFragment = new HomeFragment();
+        profileFragment = new ProfileFragment();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameContainer,homeFragment);
+        fragmentTransaction.commit();
+
+
+        mBottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+                switch (menuItem.getItemId()){
+                    case R.id.itemHome :
+                        fragment = homeFragment;
+                        break;
+                    case R.id.itemProfile :
+                        fragment = profileFragment;
+                        break;
+                }
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frameContainer,fragment);
+                fragmentTransaction.commit();
+                return true;
+            }
+        });
     }
 }
